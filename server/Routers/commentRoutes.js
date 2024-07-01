7d14ea31c03fdfcc1264a46ace254eb4c1b446f9
Mohammed Riyan <97107462+riyanrathore26@users.noreply.github.com>
2024-07-01 16:47:41 +0530

    Add files via upload

---
+++ server/Routers/commentRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const CommentModel = require('../Models/Comment');
+
+const router = express.Router();
+
+router.post('/addcomment', async (req, res) => {
+  try {
+    const { productId, comment, username } = req.body;
+    const datetime = new Date();
+
+    const newComment = new CommentModel({
+      productId,
+      comment,
+      username,
+      datetime,
+    });
+
+    await newComment.save();
+    res.status(200).send({ message: 'Comment added successfully!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
+});
+
+router.get('/showcomments/:productId', async (req, res) => {
+  const productId = req.params.productId;
+  const commentsData = await CommentModel.find({ productId });
+  res.json(commentsData);
+});
+
+module.exports = router;
