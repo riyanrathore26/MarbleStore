32c0f2c080c73ab41159a7d6ee1f02a66e12f258
Mohammed Riyan <97107462+riyanrathore26@users.noreply.github.com>
2024-07-01 16:44:30 +0530

    Delete marblestore directory

--- marblestore/client/src/config.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://marblestore-6imh.onrender.com'
-// export const BASE_URL = 'http://localhost:3000'
--- marblestore/server/config/awsConfig.js
+++
@@ -1,9 +0,0 @@
-const AWS = require('aws-sdk');
-require('dotenv').config();
-
-AWS.config.update({
-  region: process.env.AWS_REGION
-});
-const cognito = new AWS.CognitoIdentityServiceProvider();
-
-module.exports = { cognito };
