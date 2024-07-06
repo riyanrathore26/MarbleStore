49f882ebefc11312b2828effaf8e2bc9120cfe8c
Mohammed Riyan <97107462+riyanrathore26@users.noreply.github.com>
2024-07-06 16:06:44 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'https://marblestore-6imh.onrender.com'
+export const BASE_URL = 'https://marblestore-6imh.onrender.com'
 // export const BASE_URL = 'http://localhost:3000'
 // export const BASE_URL = 'https://av4olzjeu9.execute-api.ap-south-1.amazonaws.com/default/Marblestoreauth'
-export const BASE_URL = 'https://dj9i18f4oi.execute-api.ap-south-1.amazonaws.com/default/Marblestoreauth'
+// export const BASE_URL = 'https://dj9i18f4oi.execute-api.ap-south-1.amazonaws.com/default/Marblestoreauth'
