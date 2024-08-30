9a454c74960375b9867fd3f15a5935cbcd5f791a
riyanrathore <rk5098863@gmail.com>
2024-08-30 15:43:31 +0530

    dot env problem solved

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,3 @@
-export const BASE_URL = 'https://marblestore-6imh.onrender.com'
-// export const BASE_URL = 'http://localhost:3000'
-// export const BASE_URL = 'https://av4olzjeu9.execute-api.ap-south-1.amazonaws.com/default/Marblestoreauth'
-// export const BASE_URL = 'https://dj9i18f4oi.execute-api.ap-south-1.amazonaws.com/default/Marblestoreauth'
+require('dotenv').config();
+
+export const BASE_URL = process.env.BASE_URL || 'https://marblestore-6imh.onrender.com';
