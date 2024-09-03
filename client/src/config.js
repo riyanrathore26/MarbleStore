6616b83074e12c4f4514211b86e866617fdbf78b
MohammedRiyan <97107462+riyanrathore26@users.noreply.github.com>
2024-09-03 16:17:43 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ let BASE_URL;
 if (window.location.hostname === 'localhost') {
   BASE_URL = 'http://localhost:3000'; // Local development URL
 } else {
-  BASE_URL = 'https://your-production-url.com'; // Production URL
+  BASE_URL = 'https://marblestore-6imh.onrender.com'; // Production URL
 }
 
 export { BASE_URL };
